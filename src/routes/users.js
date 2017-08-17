5cbb96e5f2e1721f36ff72075750aade7632bc87
amaebe <Joppy9@users.noreply.github.com>
2017-08-16 23:11:05 -0700

    コードキレイキレイ

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,4 +1,4 @@
-const crypto = require('crypto');
+
 const User = require('../models/user');
 const UserSession = require('../models/user_session.js')
 
@@ -6,37 +6,39 @@ module.exports = function (app) {
   app.get("/signup", function (req, res) {
     res.render('signup');
   });
+
   app.post("/signup", function (req, res) {
     let email = req.body.email;
-    let pass = req.body.password;
+    let password = req.body.password;
     let nickname = req.body.nickname
 
-    let salt = crypto.randomBytes(8).toString('hex');
-    let sha512 = crypto.createHash('sha512');
-    sha512.update(salt);
-    sha512.update(pass);
-    let hash = sha512.digest('hex');
-    let user = new User({
-      nickname: nickname,
-      email: email,
-      password: hash,
-      salt: salt
-    });
-    user.save().then(() => {
-      res.redirect(302, '/login');
-    }, () => {
-      res.status(409).send('Nickname または E-mailアドレスが重複している');
+    User.create(nickname, email, password).then((user) => {
+      res.redirect('/login');
+    }).catch((err) => {
+      console.log(err);
+      res.render("signup", { error: true });
     });
   });
+
   app.get('/login', function (req, res) {
     res.render('login');
   })
+
   app.post('/login', function (req, res) {
     let email = req.body.email;
-    let pass = req.body.password;
-
-    User.collection().where({ email: email }).then((users) => {
-      if (users.length < 1) {
+    let password = req.body.password;
+    //正しいかどうかチェック
+   
+      User.authenticate(email, password).then((user) => {
+        return UserSession.create(user);
+      }).then((session) => {
+        res.cookie("session_id", session.data.id, {
+          path: "/",
+          httpOnly: true,
+          expires: new Date(Date.now() + (1000 * 60 * 60 * 24 * 30)),
+          signed: true
+        });
+      /*if (users.length < 1) {
         throw new Error("User not found");
       }
       let user = users[0];
@@ -46,24 +48,10 @@ module.exports = function (app) {
       sha512.update(pass);
       let hash = sha512.digest('hex');
       if (hash !== user.data.password) {
-        throw new Error("Password is not match")
-      }
-
-      let session = new UserSession({user_id: user.data.id});
-
-      return session.save();
-      }).then((session)=>{
-        res.cookie("session_id",session.data.id,{
-          path: "/",
-          httpOnly: true,
-          expires: new Date(Date.now() + (1000 * 60 * 60 * 24 * 30)),
-          signed: true
-        })
-      
-
-      res.redirect("/");
-    }).catch((err) => {
-      res.render("login", { error: true });
-    })
-  });
+        throw new Error("Password is not match")*/
+        res.redirect("/");
+      }).catch((err) => {
+        res.render("login", { error: true });
+      })
+    });
 }
